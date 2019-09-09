6ad804c3d0c2d59e93c4dcb97ce0dab553037bcb
joeencalada <54949743+joeencalada@users.noreply.github.com>
2019-09-09 12:25:18 -0400

    Add files via upload

---
+++ integer.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class integer {
+	public static void main(String[] args) {
+/*
+ * 
+Scanner input = new Scanner(System.in);
+System.out.println("Enter three int values");
+int num1 = input.nextInt();
+int num2 = input.nextInt();
+int num3 = input.nextInt();
+int result = num1 + num2 + num3;
+System.out.print(result);
+
+*/
+
+
+
+
+Scanner input = new Scanner(System.in);
+System.out.println("Enter four int values");
+int num1 = input.nextInt();
+int num2 = input.nextInt();
+int num3 = input.nextInt();
+int num4 = input.nextInt();
+int result = num1 * num2 * num3 * num4;
+System.out.print(result);
+
+}}
